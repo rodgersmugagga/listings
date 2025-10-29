9f1ed93cb08ae54b02d2853943260390a528b653
Rodgers Mugagga <rodgersmugagga84@gmail.com>
2025-10-30 02:44:02 +0300

    show listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -17,6 +17,9 @@ export default function Profile() {
   const [previewUrl, setPreviewUrl] = useState("");
   //const [uploading, setUploading] = useState(false);
 
+  const [showListingError, setShowListingError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
+
   useEffect(() => {
     if (file) {
       const localPreview = URL.createObjectURL(file);
@@ -115,6 +118,41 @@ const handleSignOut = () => {
 };
 
 
+const handleShowListings = async () => {
+    const userId = currentUser?.user?._id;
+      
+  try {
+    setShowListingError(false);
+    
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/api/user/listings/${userId}`, {
+  method: "GET",
+  headers: {
+    Authorization: `Bearer ${currentUser.token}`,
+  },
+});
+
+    const data = await res.json();
+
+    if (!res.ok || data.success === false) {
+      setShowListingError(true);
+      
+      console.error("Listing fetch failed:", data.message);
+      alert(data.message || "Failed get listings!");
+      return;
+    }
+
+    setUserListings(data);
+    
+
+  } catch (error) {
+
+    setShowListingError(true);
+    console.error("Listing Fetch error:", error.message);
+    
+  }
+};
+
+
 
   return (
     <div>
@@ -149,6 +187,44 @@ const handleSignOut = () => {
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
         <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
+
+        <button onClick={handleShowListings} className="text-green-700 w-full p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 mt-4">Show Listings</button>
+        <p className="text-red-700 mt-5">{showListingError ? "Error showing Listings!" : ""}</p>
+
+        {userListings && userListings.length > 0 && (
+  <div className="flex flex-col gap-4">
+    <h1 className="text-center mt-7 text-2xl font-semibold">Your Listings</h1>
+
+    {userListings.map((listing) => (
+      <div
+        key={listing._id}
+        className="border rounded-lg p-3 flex justify-between items-center gap-4"
+      >
+        <Link to={`/listing/${listing._id}`}>
+          <img
+            src={listing.imageUrls?.[0] || "/placeholder.jpg"}
+            alt="Listing cover image"
+            className="h-16 w-16 object-contain"
+          />
+        </Link>
+
+        <Link
+          className="text-slate-700 font-semibold flex-1 hover:underline truncate"
+          to={`/listing/${listing._id}`}
+        >
+          <p>{listing.name}</p>
+        </Link>
+
+        <div className="flex flex-col items-center">
+          <button className="text-red-700 uppercase">Delete</button>
+          <button className="text-green-700 uppercase">Edit</button>
+        </div>
+      </div>
+    ))}
+  </div>
+)}
+
+
     </div>
   );
 }
