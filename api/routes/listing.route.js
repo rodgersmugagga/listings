e9b72624ed9b5edee48b936bea59da695e06180f
Rodgers Mugagga <rodgersmugagga84@gmail.com>
2025-11-08 10:34:49 +0300

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, deleteListing, updateListing, getListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, getListing, getListings } from '../controllers/listing.controller.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post('/create', authMiddleware, createListing);
 router.delete('/delete/:id', authMiddleware, deleteListing);
 router.post('/update/:id', authMiddleware, updateListing);
 router.get('/get/:id', getListing);
+router.get('/get/', getListings);
 
 export default router;
